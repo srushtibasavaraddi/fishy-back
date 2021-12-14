6151e529d31c55b887ed6c822300c33cc7f2d0d4
Sridarshan <sridarshan1997@gmail.com>
2021-12-15 00:28:34 +0530

    Fixing server link

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 import React from "react";
 export const socket = socketio.connect(
-  '"https://shielded-escarpment-35022.herokuapp.com/:9000"'
+  "https://shielded-escarpment-35022.herokuapp.com/:9000"
 );
 export const SocketContext = React.createContext();
