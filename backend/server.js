79d5f3fb91470dc4127c3a745ee24b9751b8d8c1
Sridarshan <sridarshan1997@gmail.com>
2021-12-15 00:06:34 +0530

    Changing server port

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,48 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const colors = require('colors')
-const cors = require('cors')
+const app = express();
+const colors = require("colors");
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-
-const io = require('socket.io')(9000, {
-    cors: {
-      origins: ['http://localhost:3000', '*'],
-    },
-  })
-
-const GameSetup = require('./game/GameSetup')
-const GameLobby = require('./game/GameLobby')
-const GamePlay = require('./game/GamePlay')
-const GameScores = require('./game/GameScores')
-const GameResults = require('./game/GameResults')
-const GameWaitingArena = require('./game/GameWaitingArena')
-
-const onConnection = (socket) => {
-    GameSetup(io, socket)
-    GameLobby(io, socket)
-    GamePlay(io, socket)
-    GameScores(io, socket)
-    GameResults(io, socket)
-    GameWaitingArena(io, socket)
-}
-
-io.on('connection', onConnection)
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-const MODE = process.env.NODE_ENV || 'development'
+const MODE = process.env.NODE_ENV || "development";
 
-app.get('/', (req, res) => {
-  res.send('Hello World from the server!')
-})
+const io = require("socket.io")(PORT, {
+  cors: {
+    origins: ["http://localhost:3000", "*"],
+  },
+});
+
+const GameSetup = require("./game/GameSetup");
+const GameLobby = require("./game/GameLobby");
+const GamePlay = require("./game/GamePlay");
+const GameScores = require("./game/GameScores");
+const GameResults = require("./game/GameResults");
+const GameWaitingArena = require("./game/GameWaitingArena");
+
+const onConnection = socket => {
+  GameSetup(io, socket);
+  GameLobby(io, socket);
+  GamePlay(io, socket);
+  GameScores(io, socket);
+  GameResults(io, socket);
+  GameWaitingArena(io, socket);
+};
+
+io.on("connection", onConnection);
+
+app.get("/", (req, res) => {
+  res.send("Hello World from the server!");
+});
 
 app.listen(
-    PORT,
-    console.log(
-      `Server running on port ${PORT} in ${MODE.blue.bold + ' mode'.yellow.bold} `.yellow.bold
-    )
-  );
-  
+  PORT,
+  console.log(
+    `Server running on port ${PORT} in ${MODE.blue.bold + " mode".yellow.bold} `
+      .yellow.bold
+  )
+);
