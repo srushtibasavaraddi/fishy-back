4b3b65c13fe90e1d593c3072d289e93e802da1bb
Sridarshan <sridarshan1997@gmail.com>
2021-12-15 23:49:44 +0530

    Disabling long polling

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -1,6 +1,6 @@
-import socketio from "socket.io-client";
+import io from "socket.io-client";
 import React from "react";
-export const socket = socketio.connect(
-  "https://shielded-escarpment-35022.herokuapp.com/"
-);
+export const socket = io("https://shielded-escarpment-35022.herokuapp.com/", {
+  transports: ["websocket"],
+});
 export const SocketContext = React.createContext();
