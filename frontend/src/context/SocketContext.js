fc2a2b83c345a3de9cc23a0d12d83dfa791feaac
Sridarshan <sridarshan1997@gmail.com>
2021-12-20 12:30:14 +0530

    Removing transports property

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -1,6 +1,4 @@
 import io from "socket.io-client";
 import React from "react";
-export const socket = io("https://shielded-escarpment-35022.herokuapp.com/", {
-  transports: ["websocket"],
-});
+export const socket = io("https://shielded-escarpment-35022.herokuapp.com/");
 export const SocketContext = React.createContext();
