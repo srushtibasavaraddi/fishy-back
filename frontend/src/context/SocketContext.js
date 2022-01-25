4c5c0579305d2c1a7d030a4ee09dd29eee3e57ca
Maruti Arer <marutiarer16@gmail.com>
2022-01-25 20:54:27 +0000

    Update frontend/src/context/SocketContext.js

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 import React from "react";
-export const socket = io("http://54.176.18.38:5001/);
+export const socket = io("http://54.176.18.38:5001/");
 export const SocketContext = React.createContext();
